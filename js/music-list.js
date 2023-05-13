02ec5b18deecfe00d91bb33c0a7a64163748b5a5
AndreyTkachenko21 <gfhx392@gmail.com>
2023-05-13 23:28:57 +0300

    Initial commit

---
+++ js/music-list.js
@@ -0,0 +1,216 @@
+let allMusic = [
+
+    {
+        name: "Preludium...",
+        artist: "Tristania",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Evenfall",
+        artist: "Tristania",
+        img: "music-1",
+        src: "music-2"
+    },
+    {
+        name: "Pale Enchantress",
+        artist: "Tristania",
+        img: "music-1",
+        src: "music-3"
+    },
+    {
+        name: "December Elegy",
+        artist: "Tristania",
+        img: "music-1",
+        src: "music-4"
+    },
+    {
+        name: "Midwintertears",
+        artist: "Tristania",
+        img: "music-1",
+        src: "music-5"
+    },
+    {
+        name: "Angellore",
+        artist: "Tristania",
+        img: "music-1",
+        src: "music-6"
+    },
+    {
+        name: "My Lost Lenore",
+        artist: "Tristania",
+        img: "music-1",
+        src: "music-7"
+    },
+    {
+        name: "Wasteland_s Caress",
+        artist: "Tristania",
+        img: "music-1",
+        src: "music-8"
+    },
+    {
+        name: "...Postludium",
+        artist: "Tristania",
+        img: "music-1",
+        src: "music-9"
+    },
+    {
+        name: "Sirene",
+        artist: "Tristania",
+        img: "music-1",
+        src: "music-10"
+    },
+    {
+        name: "Cease to Exist",
+        artist: "Tristania",
+        img: "music-1",
+        src: "music-11"
+    },
+    {
+        name: "Handful of dust",
+        artist: "Noumena",
+        img: "noumena",
+        src: "music-12"
+    },
+    {
+      name: "Play dead",
+      artist: "Noumena",
+      img: "noumena",
+      src: "music-13"
+    },
+    {
+      name: "Sleep",
+      artist: "Noumena",
+      img: "noumena",
+      src: "music-14"
+    },
+    {
+      name: "Death walks with me",
+      artist: "Noumena",
+      img: "noumena",
+      src: "music-15"
+    },
+    {
+      name: "Let it run red",
+      artist: "Noumena",
+      img: "noumena",
+      src: "music-16"
+    },
+    {
+      name: "The storm",
+      artist: "Noumena",
+      img: "noumena",
+      src: "music-17"
+    },
+    {
+      name: "Mysteries of motion",
+      artist: "Noumena",
+      img: "noumena",
+      src: "music-18"
+    },
+    {
+      name: "Nothing",
+      artist: "Noumena",
+      img: "noumena",
+      src: "music-19"
+    },
+    {
+      name: "Only the silent",
+      artist: "Noumena",
+      img: "noumena",
+      src: "music-20"
+    },
+    {
+      name: "Season of suffocation",
+      artist: "Noumena",
+      img: "noumena",
+      src: "music-21"
+    },
+    {
+      name: "Undown",
+      artist: "Noumena",
+      img: "noumena",
+      src: "music-22"
+    },
+    {
+        name: "War Of The Gods",
+        artist: "Amon Amarth",
+        img: "cover",
+        src: "music-23"
+    },
+    {
+        name: "Tock's Taunt - Loke's Treachery, Pt. II",
+        artist: "Amon Amarth",
+        img: "cover",
+        src: "music-24"
+    },
+    {
+        name: "Destroyer Of The Universe",
+        artist: "Amon Amarth",
+        img: "cover",
+        src: "music-25"
+    },
+    {
+        name: "Slaves Of Fear",
+        artist: "Amon Amarth",
+        img: "cover",
+        src: "music-26"
+    },
+    {
+        name: "Live Without Regrets",
+        artist: "Amon Amarth",
+        img: "cover",
+        src: "music-27"
+    },
+    {
+        name: "The Last Stand Of Frej (feat. Simon Solomon)",
+        artist: "Amon Amarth",
+        img: "cover",
+        src: "music-28"
+    },
+    {
+        name: "For Victory Or Death",
+        artist: "Amon Amarth",
+        img: "cover",
+        src: "music-29"
+    },
+    {
+        name: "Wrath Of The Norsemen",
+        artist: "Amon Amarth",
+        img: "cover",
+        src: "music-30"
+    },
+    {
+        name: "A Beast Am I (feat. Simon Solomon)",
+        artist: "Amon Amarth",
+        img: "cover",
+        src: "music-31"
+    },
+    {
+        name: "Doom Over Dead Man",
+        artist: "Amon Amarth",
+        img: "cover",
+        src: "music-32"
+    },
+    {
+        name: "Balls To The Wall",
+        artist: "Amon Amarth",
+        img: "cover",
+        src: "music-33"
+    },
+    {
+        name: "War Machine",
+        artist: "Amon Amarth",
+        img: "cover",
+        src: "music-34"
+    },
+    {
+        name: "Aerials",
+        artist: "Amon Amarth",
+        img: "cover",
+        src: "music-35"
+    },
+    
+      
+    
+    ];
